4d7e9190146abfadcbf203665cebac3741535712
Daniel Yowell <danielyowell@ou.edu>
2023-02-14 00:08:37 -0600

    branched from main, added reverse function

--- hw1.py
+++ hw1.py
@@ -10,6 +10,12 @@ def product(numbers):
         product = product * numbers[x]
     return product
 
+def reverse(numbers):
+    list2 = []
+    for x in numbers:
+        list2.insert(0, x)
+    return list2
+
 def main():
     inputnum = input("input an integer (or any other symbol when done): ")
     list = []
@@ -19,6 +25,7 @@ def main():
         inputnum = input("input an integer (or any other symbol when done): ")
     print("sum: ", sum(list))
     print("product: ", product(list))
+    print("reversed list: ", reverse(list))
 
 if __name__ == '__main__':
     main()
