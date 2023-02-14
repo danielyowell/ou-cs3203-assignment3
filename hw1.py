1d9959637f44a3486f43561bc42388b8426b6412
Daniel Yowell <danielyowell@ou.edu>
2023-02-13 23:52:42 -0600

    added main function, fixed product function

--- hw1.py
+++ hw1.py
@@ -5,7 +5,20 @@ def sum(numbers):
     return sum
 
 def product(numbers):
-    product = 0
-    for x in numbers:
-        product = product * x
+    product = numbers[0]
+    for x in range(1, len(numbers)):
+        product = product * numbers[x]
     return product
+
+def main():
+    inputnum = input("input an integer (or any other symbol when done): ")
+    list = []
+    while inputnum.isdigit():
+        inputnum = int(inputnum)
+        list.append(inputnum)
+        inputnum = input("input an integer (or any other symbol when done): ")
+    print(sum(list))
+    print(product(list))
+
+if __name__ == '__main__':
+    main()
